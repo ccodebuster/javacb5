746f20a33559f3e8dfc87e4fbde925bcc7054241
doorclose <codebuster2023@gmail.com>
2024-04-07 22:34:25 +0100

    My 1st Push.

---
+++ javademo/src/dividetwonumber/Divideint.java
@@ -0,0 +1,17 @@
+package dividetwonumber;
+
+public class Divideint {
+
+    public static void main(String[] args) {
+        // Divide two number question ****  50/3=16 ****** //
+        int x  = 50;
+        int y = 3;
+
+        int  result =  x/y ;
+
+    // Result//
+
+        System.out.println("Result:-->>"+result);
+
+    }
+}
